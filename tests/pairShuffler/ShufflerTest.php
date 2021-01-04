2f46cecc2384adac90b0ba8c4e4c0f9741e9a29b
vdebes <vdebes.externe@bedrockstreaming.com>
2021-01-04 19:52:12 +0100

    test 2 participants make 1 pair

--- tests/pairShuffler/ShufflerTest.php
+++ tests/pairShuffler/ShufflerTest.php
@@ -10,20 +10,21 @@ class ShufflerTest extends TestCase
     /**
      * @dataProvider participantsDataProvider
      */
-    public function testShuffler(array $participants, int $expectedGroups): void
+    public function testShuffler(array $participants, int $expectedGroupsCount): void
     {
         $testedInstance = new Shuffler();
         self::assertIsObject($testedInstance);
         self::assertTrue(method_exists($testedInstance, 'shuffle'));
 
         self::assertIsArray($output = $testedInstance->shuffle($participants));
-        self::assertCount(count($participants), $output);
+        self::assertCount($expectedGroupsCount, $output);
     }
 
     public function participantsDataProvider(): array
     {
         return [
             [['Riri'], 1],
+            [['Riri','Fifi'], 1],
         ];
     }
 }
