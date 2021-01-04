c55cbf822bb9241422ce5dddfaa4210cb0efb16a
vdebes <vdebes.externe@bedrockstreaming.com>
2021-01-04 23:58:59 +0100

    test shuffle participants

--- tests/pairShuffler/ShufflerTest.php
+++ tests/pairShuffler/ShufflerTest.php
@@ -10,7 +10,7 @@ class ShufflerTest extends TestCase
     /**
      * @dataProvider participantsDataProvider
      */
-    public function testShuffler(array $participants, int $expectedGroupsCount): void
+    public function testSplitsParticipantsInDuoOrTrio(array $participants, int $expectedGroupsCount): void
     {
         $testedInstance = new Shuffler();
         self::assertIsObject($testedInstance);
@@ -18,14 +18,40 @@ class ShufflerTest extends TestCase
         self::assertCount($expectedGroupsCount, $output);
     }
 
+    /**
+     * @dataProvider participantsDataProvider
+     */
+    public function testOutputContainsAllParticpants(array $participants): void
+    {
+        $testedInstance = new Shuffler();
+
+        $output = $testedInstance($participants);
+
+        $flatOutput = array_merge(...$output);
+        self::assertEmpty(array_diff($participants, $flatOutput));
+    }
+
+    /**
+     * @dataProvider participantsDataProvider
+     */
+    public function testOutputOrderDiffersFromInputs(array $participants): void
+    {
+        $testedInstance = new Shuffler();
+
+        $output = $testedInstance($participants);
+
+        $flatOutput = array_merge(...$output);
+        self::assertNotEquals($participants, $flatOutput);
+    }
+
     public function participantsDataProvider(): array
     {
         return [
-            [['Riri'], 1],
-            [['Riri','Fifi'], 1],
-            [['Riri','Fifi','Loulou'], 1],
-            [['Riri','Fifi','Loulou','Donald'], 2],
-            [['Riri','Fifi','Loulou','Donald','Picsou'], 2],
+            'Riri' => [['Riri'], 1],
+            'Riri,Fifi' => [['Riri','Fifi'], 1],
+            'Riri,Fifi,Loulou' => [['Riri','Fifi','Loulou'], 1],
+            'Riri,Fifi,Loulou,Donald' => [['Riri','Fifi','Loulou','Donald'], 2],
+            'Riri,Fifi,Loulou,Donald,Picsou' => [['Riri','Fifi','Loulou','Donald','Picsou'], 2],
         ];
     }
 }
